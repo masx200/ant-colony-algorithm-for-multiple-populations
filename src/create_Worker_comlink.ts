16a52c8beb48e043b5e9e4a35fd01fff554e058d
masx200 <masx200@qq.com>
2022-11-07 13:26:29 +0800

    Update create_Worker_comlink.ts

--- src/create_Worker_comlink.ts
+++ src/create_Worker_comlink.ts
@@ -2,7 +2,7 @@ import * as comlink from "comlink";
 
 export function create_Worker_comlink<API>(
     createWorker: () => Worker,
-    error_listener: (this: Worker, ev: ErrorEvent) => any
+    error_listener: (this: Worker, ev: ErrorEvent) => void
 ): { remote: comlink.Remote<API>; worker: Worker } & { terminate: () => void } {
     const endpoint = createWorker();
     const remote = comlink.wrap<API>(endpoint);
