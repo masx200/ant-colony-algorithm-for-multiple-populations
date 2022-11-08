e6c7352f20515f9a7df6d407b8563cb2b803cc32
masx200 <masx200@qq.com>
2022-11-08 16:37:31 +0800

    不再使用proxy

--- src/TSP_Worker_Remote.ts
+++ src/TSP_Worker_Remote.ts
@@ -1,7 +1,7 @@
-import * as comlink from "comlink";
+import { RemoteObject, ProxyMethods } from "comlink";
 
 import { TSP_Worker_API } from "./TSP_Worker_API";
 
 export type TSP_Worker_Remote = { worker: Worker; terminate: () => void } & {
-    remote: comlink.RemoteObject<TSP_Worker_API> & comlink.ProxyMethods;
+    remote: RemoteObject<TSP_Worker_API> & ProxyMethods;
 };
