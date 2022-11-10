45316d56a1fdeabde203d43bde72686e5e3077b2
masx200 <masx200@qq.com>
2022-11-10 16:33:54 +0800

    Update MultiPopulationScheduler.ts

--- classic-acs/MultiPopulationScheduler.ts
+++ classic-acs/MultiPopulationScheduler.ts
@@ -12,11 +12,27 @@ export function MultiPopulationScheduler(
     input: TSPRunnerOptions
 ): MultiPopulationScheduler {
     const options = Object.assign(structuredClone(DefaultOptions), input);
+    const {
+        number_of_populations_of_the_first_category,
+        number_of_the_second_type_of_population,
+        node_coordinates,
+        population_communication_iterate_cycle,
+    } = options;
     return {
-        async runIterations(iterations: number) {},
-        async runOneIteration() {},
-        getBestLength(): number {},
-        getBestRoute(): number[] {},
-        getOutputDataAndConsumeIterationData(): COMMON_TSP_Output {},
+        async runIterations(iterations: number) {
+            throw Error("not implemented");
+        },
+        async runOneIteration() {
+            throw Error("not implemented");
+        },
+        getBestLength(): number {
+            throw Error("not implemented");
+        },
+        getBestRoute(): number[] {
+            throw Error("not implemented");
+        },
+        getOutputDataAndConsumeIterationData(): COMMON_TSP_Output {
+            throw Error("not implemented");
+        },
     };
 }
