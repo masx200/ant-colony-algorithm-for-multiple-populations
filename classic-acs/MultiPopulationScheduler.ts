ecdf0ae94d64920852fe2ebf25cd8b968da07405
masx200 <masx200@qq.com>
2022-11-15 14:38:46 +0800

    Update MultiPopulationScheduler.ts

--- classic-acs/MultiPopulationScheduler.ts
+++ classic-acs/MultiPopulationScheduler.ts
@@ -1,3 +1,10 @@
 import { CommonTspRunner } from "./CommonTspRunner";
+import { COMMON_DataOfOneIteration, COMMON_TSP_Output } from "./tsp-interface";
 
-export type MultiPopulationScheduler = CommonTspRunner;
+export interface MultiPopulationScheduler extends CommonTspRunner {
+    getOutputDataAndConsumeIterationAndRouteData: () => Promise<
+        COMMON_TSP_Output & {
+            IterationDataForIndividualPopulations: COMMON_DataOfOneIteration[][];
+        }
+    >;
+}
