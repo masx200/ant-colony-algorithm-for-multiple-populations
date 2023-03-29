6d46c719c90c34b0b3cd9e3d52f566b57cf8ba27
masx200 <34191203+masx200@users.noreply.github.com>
2023-03-29 14:06:02 +0800

    Update PureDataOfFinishOneRoute.ts

--- functions/PureDataOfFinishOneRoute.ts
+++ functions/PureDataOfFinishOneRoute.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
 import { DataOfFinishOneRoute } from "./DataOfFinishOneRoute.ts";
 
 export type PureDataOfFinishOneRoute = Omit<
